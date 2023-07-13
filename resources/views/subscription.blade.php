143cf783ac712e168a09663cb8c890406311da24
alihumdard <alihumdard125@gmail.com>
2023-07-13 12:05:57 +0500

    bugs fixing additional changes

--- resources/views/subscription.blade.php
+++ resources/views/subscription.blade.php
@@ -1,49 +1,4 @@
-<!DOCTYPE html>
-<html lang="en">
-
-<head>
-  <meta charset="UTF-8">
-  <meta http-equiv="X-UA-Compatible" content="IE=edge">
-  <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <title>Document</title>
-
-  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/css/bootstrap.min.css" rel="stylesheet"
-    integrity="sha384-KK94CHFLLe+nY2dmCWGMq91rCGa5gtU4mk92HdvYe+M/SXH301p5ILy+dN9+nJOZ" crossorigin="anonymous">
-  <link rel="stylesheet" href="assets/csases/style.css">
-</head>
-
-<body>
-  <!-- ======================navbar-start====================== -->
-  <nav class="navbar navbar-expand-lg  m-0 p-0">
-    <div class="container-fluid">
-      <h1 class="ms-3"><img src="assets/images/logo.svg" alt=""></h1>
-      <button class=" navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent"
-          aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-        <span class="navbar-toggler-icon"></span>
-        </button>
-        <div class="collapse navbar-collapse" id="navbarSupportedContent">
-          <ul class="navbar-nav ms-auto mb-2 mb-lg-0">
-            <li class="nav-item active">
-              <a style="color: #3A0CA3; font-weight: 700; font-size:1.0625rem; margin-right: 4rem;" class="nav-link "
-                href="#">What is it? </a>
-            </li>
-            <li class="nav-item">
-              <a style="color: #ACADAE; font-weight: 500; font-size:1.0625rem;margin-right: 4rem;"" class=" nav-link"
-                href="#">Home </a>
-            </li>
-            <li class="nav-item">
-              <a style="color: #ACADAE; font-weight: 500; font-size:1.0625rem;margin-right: 4rem;"" class=" nav-link"
-                href="#">About </a>
-            </li>
-            <li class="nav-item ">
-              <a style="color: #ACADAE; font-weight: 500; font-size:1.0625rem;margin-right: 3rem;"" class=" nav-link"
-                href="#">Price </a>
-            </li>
-          </ul>
-        </div>
-    </div>
-  </nav>
-  <!-- ======================navbar-start====================== -->
+@include('web_nav')
   <!-- ======================Hero-Section-start====================== -->
   <section id="home">
 
@@ -138,8 +93,8 @@
           <div class="col-md-6">
             <div class="">
               <div class="d-flex flex-column align-items-center ">
-                <button style="width:150px ; height:50px ; background: #452C88; box-shadow: 1px 4px 20px 2px #00000040;"
-                  class="btn text-white">
+                <button style="width:150px ; opacity: 65%; height:50px ; background: #452C88; box-shadow: 1px 4px 20px 2px #00000040;"
+                  class="btn text-white" data-toggle="tooltip" data-placement="right" title="Not available Yet!">
                   <img src="assets/images/Pay Vaya.svg" style="width: 70%; height: 70%;" alt="">
                 </button>
 
@@ -155,26 +110,26 @@
            
 
                 <button
-                  style="width:150px ; height:50px ; background: #E6F6FF; box-shadow: 1px 4px 20px 2px #00000040;margin-top: 2.5rem;"
-                  class="btn text-white">
+                  style="width:150px ; height:50px; opacity: 65%; background: #E6F6FF; box-shadow: 1px 4px 20px 2px #00000040;margin-top: 2.5rem;"
+                  class="btn text-white"  data-toggle="tooltip" data-placement="right" title="Not available Yet!" >
                   <img src="assets/images/gpay.svg" style="width: 70%; height: 70%;" alt="">
                 </button>
 
                 <button
-                  style="width:150px ; height:50px ; background: #FFFFFF; box-shadow: 1px 4px 20px 2px #00000040;margin-top: 2.5rem;"
-                  class="btn text-white">
+                  style="width:150px ; height:50px; opacity: 65%; background: #FFFFFF; box-shadow: 1px 4px 20px 2px #00000040;margin-top: 2.5rem;"
+                  class="btn text-white" data-toggle="tooltip" data-placement="right" title="Not available Yet!" >
                   <img src="assets/images/visa.svg" style="width: 70%; height: 70%;" alt="">
                 </button>
 
                 <button
-                  style="width:150px ; height:50px ; background: #F8F8F8; box-shadow: 1px 4px 20px 2px #00000040;margin-top: 2.5rem;"
-                  class="btn text-white">
+                  style="width:150px ; height:50px; opacity: 65%; background: #F8F8F8; box-shadow: 1px 4px 20px 2px #00000040;margin-top: 2.5rem;"
+                  class="btn text-white" data-toggle="tooltip" data-placement="right" title="Not available Yet!" >
                   <img src="assets/images/mastercard.svg" style="width: 70%; height: 70%;" alt="">
                 </button>
 
                 <button
-                  style="width:150px ; height:50px ; background:#E45F00; box-shadow: 1px 4px 20px 2px #00000040;margin-top: 2.5rem;"
-                  class="btn text-white">
+                  style="width:150px ; height:50px; opacity: 65%; background:#E45F00; box-shadow: 1px 4px 20px 2px #00000040;margin-top: 2.5rem;"
+                  class="btn text-white" data-toggle="tooltip" data-placement="right" title="Not available Yet!" >
                   <img src="assets/images/Bank Transfer.svg" style="width: 70%; height: 70%;" alt="">
                 </button>
 
@@ -269,6 +224,7 @@
   <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/js/bootstrap.bundle.min.js"
     integrity="sha384-ENjdO4Dr2bkBIFxQpeoTz1HIcje39Wm4jDKdf19U8gI4ddQ3GYNS7NTKfAdVQSZe"
     crossorigin="anonymous"></script>
+    <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
 </body>
 <script>
   const tooltipTriggerList = document.querySelectorAll('[data-bs-toggle="tooltip"]')
@@ -278,5 +234,11 @@ const tooltipList = [...tooltipTriggerList].map(tooltipTriggerEl => new bootstra
   function submitPaypalForm() {
     document.getElementById('paypalForm').submit();
   }
+
+  $(function() {
+  $('[data-toggle="tooltip"]').tooltip({
+    placement: 'right'
+  });
+});
 </script>
 </html>
